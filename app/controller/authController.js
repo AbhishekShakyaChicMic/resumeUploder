cb4e2c2522625034a72193ee4f548923edd5d513
AbhishekShakyaChicMic <abhishekh.kumar@chicmicstudios.in>
2025-04-23 18:19:27 +0530

    fix(Api): Fixed Api Structure

--- app/controller/authController.js
+++ app/controller/authController.js
@@ -11,11 +11,15 @@ const authControllers = {};
 authControllers.signup = async (payload) => {
     const { name, email, password, mobile } = payload;
     
+    //find email exits or not in database
     const user =await dbServices.findOneData(userModel,{email:email})
     
+    //if user exist and user is not deleted then throw error response
     if (user && user.isDeleted !== true) {
         throw createFailResponse(message.USERID_ALREADY_EXISTS, "ALREADY_EXISTS");
     }
+
+    //hash the password
     const hash = await utils.hashPassword(password);
     await dbServices.updateOneData(userModel, { email: email }, { name, email, password: hash, mobile, isDeleted:false }, { upsert: true });
     
@@ -24,15 +28,22 @@ authControllers.signup = async (payload) => {
 }
 
 authControllers.login = async (payload) => {
+
     const { email, password } = payload;
+
     const user = await dbServices.findOneData(userModel, { email: email });
+
+    //find User exiSts or not if exists then it is deleted or not 
     if (!user || user.isDeleted) {
         throw createFailResponse(message.USER_NOT_REGISTERED, "DATA_NOT_FOUND");
     }
+
+    //check provided password is write or wrong 
     if (!await utils.compareHash(password, user.password)) {
         throw createFailResponse(message.WRONG_PASSWORD, "FORBIDDEN");
     }
     
+    //make to token fist access token and refresh token
     const accessToken = utils.encryptJwt({ userId: user._id, date: uuidv4() },JWT_ACCESS_KEY, '20m');
     const refreshToken = utils.encryptJwt({ userId: user._id,date:uuidv4()},JWT_REFRESS_KEY, '1d');
     
--- app/controller/userController.js
+++ app/controller/userController.js
@@ -16,24 +16,30 @@ userControllers.getProfileDetailsById = async (payload) => {
 
     const user1 = await dbServices.findOneData(userModel, { _id: payload.user.userId });
 
+    //compair provided userId to Auth validate userId if both is not same and user not admin then throw error
     if ((id.toString() !== payload.user.userId.toString()) && user1.role !== 'admin') {
         throw createFailResponse(message.FORBIDDEN, "FORBIDDEN");
     }
 
     const user = await dbServices.findOneData(userModel, { _id: id });
 
+    //Extract require data from user and send as a responce
     const data = {
         name: user.name,
         email: user.email,
         mobile: user.mobile,
     }
+
+    //make redis key and store in redis database
     const cacheKey = `${id}`;
     await redisClient.setEx(cacheKey, CONST.REDIS_TTL, JSON.stringify(data));
+
     const result = createSuccessResponseWithStatus(message.SUCCESS, data);
     return result;
 }
 
 userControllers.updateProfile = async (payload) => {
+    //Collect data that requested by user to updat 
     const data = {};
     if (payload.name) {
         data.name = payload.name;
@@ -41,31 +47,44 @@ userControllers.updateProfile = async (payload) => {
     if (payload.mobile) {
         data.mobile = payload.mobile;
     }
+
     const cacheKey = `${id}`;
 
+    //update data in data base as per user requiest
     const feedBack = await dbServices.updateOneData(userModel, { _id: payload.user.userId }, { $set: data }, { upsert: false });
     if (feedBack.matchedCount === 0) {
         throw createFailResponse(message.NOT_FOUND, "NOT_FOUND");
     }
+
+    //deleted store data from cache memory
     await redisClient.del(cacheKey);
-    const user = await findOneData(userModel, { _id: payload.user.userId });
-    await redisClient.setEx(cacheKey, CONST.REDIS_TTL, JSON.stringify(user));
+    
     const result = createSuccessResponseWithStatus(message.SUCCESS);
     return result;
 }
 
 userControllers.deleteProfileById = async (payload) => {
+
     const { id } = payload;
+
+    //redis key for Store profile data
     const cacheKey = `${id}`
+
     const user = await dbServices.findOneData(userModel, { _id: payload.user.userId });
+
+    //check the user is valid if not then it is not admin
     if ((id.toString() !== payload.user.userId.toString()) && user.role !== 'admin') {
         throw createFailResponse(message.FORBIDDEN, "FORBIDDEN");
     }
+
     const feedBack = await dbServices.updateOneData(userModel, { _id: id }, { $set: { isDeleted: true } }, { upsert: false });
     if (feedBack.matchedCount === 0) {
         throw createFailResponse(message.NOT_FOUND, "NOT_FOUND");
     }
+
+    //deleted data from  redis database
     await redisClient.del(cacheKey);
+
     const result = createSuccessResponseWithStatus(message.SUCCESS);
     return result;
 }
@@ -75,6 +94,7 @@ userControllers.forgetPassword = async (payload) => {
     try {
         const user = await dbServices.findOneData(userModel, { email: email });
 
+        //check user exists or not
         if (!user || user.isDeleted) {
             throw createFailResponse(message.USER_NOT_REGISTERED, "DATA_NOT_FOUND");
         }
@@ -84,6 +104,7 @@ userControllers.forgetPassword = async (payload) => {
         //server url where backend code is hosted
         const resetURL = `http://localhost:5555/changePassword?token=${token}`;
 
+        //send this data to user email
         const mailOptions = {
             from: 'abhi75191112@gmail.com',
             to: email,
@@ -93,6 +114,7 @@ userControllers.forgetPassword = async (payload) => {
            <a href="${resetURL}">${resetURL}</a>`,
         };
         await transporter1.sendMail(mailOptions);
+
         const result = createSuccessResponseWithStatus(message.SUCCESS);
         return result;
     } catch (err) {
@@ -102,17 +124,24 @@ userControllers.forgetPassword = async (payload) => {
 
 userControllers.changePassword = async (payload) => {
     const { password, token } = payload;
+
+    //check Token is given or not 
     if (!token) {
         throw createFailResponse(message.TOKEN_NOT_AVAIL, "DATA_NOT_FOUND");
     }
+
     try {
         const ans = await utils.decryptJwt(token);
 
+        //Hash the password that provided by user
         const pass = await utils.hashPassword(password);
+
+        //update password in database
         const feedBack = await dbServices.updateOneData(userModel, { _id: ans.userId }, { $set: { password: pass } }, { upsert: false });
         if (feedBack.matchedCount === 0) {
             throw createFailResponse(message.NOT_FOUND, "DATA_NOT_FOUND");
         }
+
         const result = createSuccessResponseWithStatus(message.CHANGE_PASSWORD);
         return result;
     } catch (err) {
@@ -121,6 +150,7 @@ userControllers.changePassword = async (payload) => {
 }
 
 userControllers.getAllUserProfile = async (payload) => {
+
     const { page, limit } = payload;
     try {
         const skip = (page - 1) * limit;
@@ -135,22 +165,18 @@ userControllers.getAllUserProfile = async (payload) => {
 userControllers.createNewRefreshToken = async (payload) => {
     const { token } = payload;
     try {
+        //verify the refresh token
         const verify = await utils.decryptJwt(token, JWT_REFRESS_KEY);
-        console.log(verify);
-        const cacheKey = `${verify.userId}` + 'refreshToken';
 
-        const redisReftoken = await redisClient.get(cacheKey);
-        
-        if (!await utils.compareHash(token, redisReftoken)) {
-            throw createFailResponse(message.INVALID_REFRESH_TOKEN, "FORBIDDEN");
-        }
-
+        //find user used the refresh token
         const user = await dbServices.findOneData(userModel, { _id: verify.userId });
 
-
+        //check user exists if exists then is deleted or not
         if (!user || user.isDeleted) {
             throw createFailResponse(message.USER_NOT_REGISTERED, "DATA_NOT_FOUND");
         }
+
+        //make access token and give it back to user
         const accessToken = await utils.encryptJwt({ userId: user._id, date: uuidv4() }, JWT_ACCESS_KEY, "1h");
 
         const data = {
